a8eea2a9da092c4b2b2f31d34772192b696ec0b5
lynda-trad <lynda.trad@laplateforme.io>
2021-11-18 15:08:46 +0100

    better legend for graphs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import folium as folium
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -10,11 +11,14 @@ def cleaningColumns(dataF):
     dataF.drop('numero', axis=1, inplace=True)
     dataF.drop('id_emplacement', axis=1, inplace=True)
     dataF.drop('variete', axis=1, inplace=True)
+    """
     dataF.drop('geo_point_2d_a', axis=1, inplace=True)
     dataF.drop('geo_point_2d_b', axis=1, inplace=True)
+    """
     dataF.drop('stade_developpement', axis=1, inplace=True)
     dataF.drop('remarquable', axis=1, inplace=True)
 
+    # Removing columns that have 20 percent of data missing == NaN
     total_trees = getTreeNumber(data)
     nan = dataF.isnull().sum()
 
@@ -62,30 +66,11 @@ data = cleaningRows(data)
 # Important values
 columns = getColumns(data)
 row_count = getTreeNumber(data)
+PARIS_LOCATION = (48.856614, 2.3522219)
 
 # Save cleaned up csv
-data.to_csv('./resources/cleanedDF.csv', encoding='utf-8', sep=';')  # index = False to remove previous index column
+data.to_csv('./resources/cleanedDF.csv', encoding='utf-8', sep=';', index=False)
 data.reset_index(drop=True)
-
-# Get all rows with judee as libelle_francais
-index = data.index
-new_val = data["libelle_francais"] == 'Arbre de Judée'
-b = index[new_val]
-judee_rows = b.tolist()
-print("Get only rows with Arbre de Judée:", judee_rows)
-print('\n', data["libelle_francais"][b[0]])
-
-# Groupby libelle_francais
-categories_grp = data.groupby('libelle_francais')
-total_categories = len(categories_grp)
-print("Tree categories:\n", total_categories, '\n')
-print("libelle_francais grp size:\n", categories_grp.size(), '\n')
-
-# Groupby species
-species_grp = data.groupby('espece')
-total_species = len(species_grp)
-print("Species categories:", total_species)
-print("species grp size:\n", species_grp.size(), '\n')
 
 # Top 10 most present species in Paris
 species_group = data.assign(dummy=1).groupby(['dummy', 'espece']).size(). \
@@ -97,6 +82,7 @@ plt.savefig("./resources/species_percentage.png")
 plt.show()
 
 # Tree number percentage per 'arrondissement'
+# On a wheel
 arron_group = data.assign(dummy=1).groupby(['dummy', 'arrondissement']).size(). \
     groupby(level=0).apply(lambda x: 100 * x / x.sum()).sort_values(ascending=False)
 arron_group.plot(kind='pie', subplots=True, startangle=90, figsize=(15, 10), autopct='%1.2f%%')
@@ -105,7 +91,17 @@ plt.ylabel('')
 plt.savefig("./resources/arrondissement_percentage.png")
 plt.show()
 
+# On a map
+
+m = folium.Map(location=PARIS_LOCATION, width=750, height=500)
+m
+marker = folium.Marker(
+    location=(48.8576199541,2.3209621099),
+    popup="<stong>Marker Test</stong>")
+marker.add_to(m)
+
 # Tree number percentage per 'lieu'
+# On a wheel
 lieu_group = data.assign(dummy=1).groupby(['dummy', 'lieu']).size(). \
     groupby(level=0).apply(lambda x: 100 * x / x.sum()).sort_values(ascending=False)
 lieu_group.head(10).plot(kind='pie', subplots=True, startangle=90, figsize=(15, 10), autopct='%1.2f%%')
@@ -114,10 +110,13 @@ plt.ylabel('')
 plt.savefig("./resources/top_ten_lieu.png")
 plt.show()
 
+# On a map
+
 # Height average per arrondissement
 hauteur_mean = data.groupby(['arrondissement'])['hauteur_m'].mean().reset_index().plot(x='arrondissement',
                                                                                        y='hauteur_m',
-                                                                                       kind='bar', subplots=True,
+                                                                                       kind='bar',
+                                                                                       subplots=True,
                                                                                        figsize=(15, 10))
 plt.title('Average height per arrondissement in meters')
 plt.ylabel('')
@@ -127,7 +126,8 @@ plt.show()
 # Circumference average per arrondissement
 circum_mean = data.groupby(['arrondissement'])['circonference_cm'].mean().reset_index().plot(x='arrondissement',
                                                                                              y='circonference_cm',
-                                                                                             kind='bar', subplots=True,
+                                                                                             kind='bar',
+                                                                                             subplots=True,
                                                                                              figsize=(15, 10))
 plt.title('Average circumference per arrondissement in centimeters')
 plt.ylabel('')
